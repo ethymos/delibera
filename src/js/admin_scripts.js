b64786cbcbbfcbb33965923307d7eccff0ec0905
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-05-17 16:13:41 -0300

    nao adiciona datepicker quando os inputs estiverem readonly (#15)

--- src/js/admin_scripts.js
+++ src/js/admin_scripts.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function()
 {
-	jQuery('.hasdatepicker').datepicker({
+	jQuery('.hasdatepicker:not([readonly])').datepicker({
 		userLang	: 'pt-BR',
 		americanMode: false,
 	}); 				
