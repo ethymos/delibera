d082af0bd6881b4df51775c56c88b90e063b57e3
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-06-19 15:56:56 -0300

    pequeno ajuste no tema hacklab

--- src/themes/hacklab/loop-pauta.php
+++ src/themes/hacklab/loop-pauta.php
@@ -16,12 +16,7 @@
             <h1><a href=""><?php the_title(); ?></a></h1>
             <p class="meta">Discussão criada por <span class="author"><a class="url fn n" href="<?php the_author_meta('user_url'); ?>" title="<?php printf('Ver o perfil de %s', get_the_author()); ?>"><?php the_author(); ?></a></span> em <span class="date"><?php the_date('d/m/y'); ?></span></p>
             <div class="content"><?php the_content(); ?></div>
-            <div class="actions textcenter">
-                <h2>Você quer ver essa pauta posta em discussão?</h2>
-                <button class="btn btn-success">Sim</button> <button class="btn btn-danger">Não</button>
-            </div>
+            <?php comments_template( '', true ); ?>
         </div>
-				<?php comments_template( '', true ); ?>
-
     <?php endwhile; ?>
 <?php endif; ?>
