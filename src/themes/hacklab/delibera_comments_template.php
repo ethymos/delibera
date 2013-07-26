4dda05e159706ffa031eff40d0a50c64b3b0783b
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-07-26 13:22:12 -0700

    exibe as opcoes para relatoria dos encaminhamentos mesmo quando um encaminhamento ja foi selecionado por outro relator

--- src/themes/hacklab/delibera_comments_template.php
+++ src/themes/hacklab/delibera_comments_template.php
@@ -161,7 +161,7 @@ class Delibera_Walker_Comment extends Walker_Comment
                             }
                         }
                         
-                        if ($tipo == "encaminhamento" && current_user_can('relatoria') && $situacao->slug == "relatoria") {
+                        if (($tipo == "encaminhamento" || $tipo == 'encaminhamento_selecionado') && current_user_can('relatoria') && $situacao->slug == "relatoria") {
                             $selecionados = get_post_meta($comment->comment_post_ID, '_usar_na_votacao', true);
                             
                             if (!$selecionados) {
