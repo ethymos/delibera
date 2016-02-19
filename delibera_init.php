6cacae327dcebf4fdd59d9c426a36e173c3296e0
Lucas Pirola <lucaspirola@gmail.com>
2016-02-19 09:54:11 -0200

    Add required plugins to enable attchment to comments

--- delibera_init.php
+++ delibera_init.php
@@ -333,7 +333,7 @@ add_action('wp_footer', 'delibera_footer');
 
 function delibera_loaded() {
 	// load plugin translations
-	load_plugin_textdomain('delibera', false, dirname(plugin_basename( __FILE__ )).'/lang');
+	load_plugin_textdomain('delibera', false, dirname(plugin_basename( __FILE__ )).'/languages');
 }
 add_action('plugins_loaded','delibera_loaded');
 
@@ -440,3 +440,52 @@ function delibera_convert_situacao_id_to_taxonomy_term_in_query(&$query)
 	}
 }
 add_filter('parse_query','delibera_convert_situacao_id_to_taxonomy_term_in_query');
+
+/**
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once dirname( __FILE__ ) . '/includes/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'delibera_register_required_plugins' );
+/**
+ * Register the required plugins for this theme.
+ *
+ * In this example, we register five plugins:
+ * - one included with the TGMPA library
+ * - two from an external source, one from an arbitrary source, one from a GitHub repository
+ * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
+ *
+ * The variable passed to tgmpa_register_plugins() should be an array of plugin
+ * arrays.
+ *
+ * This function is hooked into tgmpa_init, which is fired within the
+ * TGM_Plugin_Activation class constructor.
+ */
+function delibera_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+		array(
+			'name'      => 'comment-attachment',
+			'slug'      => 'comment-attachment',
+			'required'  => true
+		),
+    );
+
+	$config = array(
+		'id'           => 'delibera',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'plugins.php',            // Parent menu slug.
+		'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+    );
+
+	tgmpa( $plugins, $config );
+}
