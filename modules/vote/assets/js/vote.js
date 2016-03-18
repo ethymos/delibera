52e20cf350e344a12a9de2d1f8168b10368417dd
Jacson Passold <jacson@ethymos.com.br>
2016-03-18 03:30:08 -0300

    fix add value after delete button layout change

--- modules/vote/assets/js/vote.js
+++ modules/vote/assets/js/vote.js
@@ -1,8 +1,9 @@
 function delibera_add_comment_input(element)
 {
 	var panel = jQuery(element).parent();
-	panel.find('.delibera_comment_add_current').append(jQuery('<p><textarea name="delibera_comment_add_list[]" ></textarea><a href="#" class="delibera_comment_input_bt_remove delibera-icon-cancel"></a></p>').val(panel.find('.delibera_comment_input_list').val()));
+	panel.find('.delibera_comment_add_current').append(jQuery('<p><textarea name="delibera_comment_add_list[]" >'+panel.find('.delibera_comment_input_list').val()+'</textarea><a href="#" class="delibera_comment_input_bt_remove delibera-icon-cancel"></a></p>'));
 	panel.find('.delibera_comment_input_list').val('');
+	panel.find('.delibera_comment_input_list').focus();
 }
 
 jQuery(document).ready(function() {
