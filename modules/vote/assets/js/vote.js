9c4968ebab86e8e944116c2523034ff2116a3c05
Jacson Passold <jacson@ethymos.com.br>
2016-03-22 00:28:40 -0300

    check if add vote options is needed

--- modules/vote/assets/js/vote.js
+++ modules/vote/assets/js/vote.js
@@ -6,6 +6,25 @@ function delibera_add_comment_input(element)
 	panel.find('.delibera_comment_input_list').focus();
 }
 
+function delibera_check_discussion()
+{
+	var boxes = jQuery('#delibera-flow-column2').find('.dragbox');
+	for (var i = 0; i < boxes.length; i++)
+	{
+	  if(jQuery(boxes[i]).hasClass("emvotacao"))
+	  {
+		  jQuery('.delibera_comment_list_panel').show();
+		  return false;
+	  }
+	  if(jQuery(boxes[i]).hasClass("discussao"))
+	  {
+		  jQuery('.delibera_comment_list_panel').hide();
+		  return true;
+	  }
+	}
+	return true;
+}
+
 jQuery(document).ready(function() {
 	jQuery('.delibera_comment_input_bt_remove').live('click', function() {
 		var sel = confirm('Do you want to delete this vote option?');
@@ -14,4 +33,9 @@ jQuery(document).ready(function() {
 		}
 	    return false;
 	});
+	jQuery('#delibera-flow-column2').on('deliberaUpdateFlow', function(event, data)
+	{
+		delibera_check_discussion();
+	});
+	delibera_check_discussion();
 });
