62742f172f1e9e9a535aeccfea6a915ba3fe174f
Jacson Passold <jacson@ethymos.com.br>
2016-05-05 11:30:51 -0300

    force Debug becausa of DIVI

--- themes/generic/functions.php
+++ themes/generic/functions.php
@@ -1,7 +1,10 @@
 <?php
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+if(defined(WP_DEBUG) && WP_DEBUG)
+{
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL & ~E_STRICT);
+}
 
 function get_delibera_header() {
     $opt = delibera_get_config();
