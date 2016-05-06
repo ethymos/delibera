97c40d24c96397767ed6d359fe799891b91a52bf
Lucas Pirola <lucaspirola@gmail.com>
2016-05-06 11:10:15 -0300

    Fix url to execute ajax and remove extra icon at unlinke

--- js/delibera_concordar.js
+++ js/delibera_concordar.js
@@ -1,38 +1,38 @@
 jQuery(document).ready(function() {
 
-    jQuery(".delibera_like").click(function() {
-        var container = jQuery(this);
-        if(jQuery(this).children('input[name="object_id"]').val() > 0)
+  jQuery(".delibera_like").click(function() {
+    var container = jQuery(this);
+    if(jQuery(this).children('input[name="object_id"]').val() > 0)
+    {
+      jQuery.post(
+        delibera.ajax_url,
         {
-	        jQuery.post(
-	        	divi_delibera_concordar.ajax_url,
-	            {
-	                action : "delibera_curtir",
-	                like_id : jQuery(this).children('input[name="object_id"]').val(),
-	                type : jQuery(this).children('input[name="type"]').val(),
-	            },
-	            function(response) {
-	                jQuery(container).parent().children('.delibera-like-count').text(response);
-	            }
-	        );
+          action : "delibera_curtir",
+          like_id : jQuery(this).children('input[name="object_id"]').val(),
+          type : jQuery(this).children('input[name="type"]').val(),
+        },
+        function(response) {
+          jQuery(container).parent().children('.delibera-like-count').text(response);
         }
-    });
+      );
+    }
+  });
 
-    jQuery(".delibera_unlike").click(function() {
-        var container = jQuery(this);
-        if(jQuery(this).children('input[name="object_id"]').val() > 0)
+  jQuery(".delibera_unlike").click(function() {
+    var container = jQuery(this);
+    if(jQuery(this).children('input[name="object_id"]').val() > 0)
+    {
+      jQuery.post(
+        delibera.ajax_url,
         {
-	        jQuery.post(
-	        	divi_delibera_concordar.ajax_url,
-	            {
-	                action : "delibera_discordar",
-	                like_id : jQuery(this).children('input[name="object_id"]').val(),
-	                type : jQuery(this).children('input[name="type"]').val(),
-	            },
-	            function(response) {
-	                jQuery(container).parent().children('.delibera-unlike-count').text(response);
-	            }
-	        );
+          action : "delibera_discordar",
+          like_id : jQuery(this).children('input[name="object_id"]').val(),
+          type : jQuery(this).children('input[name="type"]').val(),
+        },
+        function(response) {
+          jQuery(container).parent().children('.delibera-unlike-count').text(response);
         }
-    });
+      );
+    }
+  });
 });
