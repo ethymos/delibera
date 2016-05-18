25072870db9555435f77e24a1acb44a19efcdd3c
Jacson Passold <jacson@ethymos.com.br>
2016-05-18 12:29:30 -0300

    api situacao

--- includes/api/wp-api.php
+++ includes/api/wp-api.php
@@ -1,29 +1,31 @@
 <?php
+add_action('rest_api_init', 'slug_register_situacao');
 
-<?php
-add_action( 'rest_api_init', 'slug_register_starship' );
-function slug_register_starship() {
-    register_rest_field( 'post',
-        'starship',
-        array(
-            'get_callback'    => 'slug_get_starship',
-            'update_callback' => null,
-            'schema'          => null,
-        )
-    );
+function slug_register_situacao()
+{
+	register_rest_field('pauta', 'situacao', 
+			array(
+				'get_callback' => 'slug_get_situacao',
+				'update_callback' => null,
+				'schema' => null
+			));
 }
 
 /**
  * Get the value of the "situação" field
  *
- * @param array $object Details of current .
- * @param string $field_name Name of field.
- * @param WP_REST_Request $request Current request
- *
+ * @param array $object
+ *        	Details of current .
+ * @param string $field_name
+ *        	Name of field.
+ * @param WP_REST_Request $request
+ *        	Current request
+ *        	
  * @return mixed
  */
-function slug_get_starship( $object, $field_name, $request ) {
-    return 	get_class(delibera_get_situacao($post->ID)?;
+function slug_get_situacao($object, $field_name, $request)
+{
+	return delibera_get_situacao( $object[ 'id' ] )->slug;
 }
 
 
