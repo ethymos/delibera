31111e6dbe63965c750f1f3b0a0c1a8407aeb690
Jacson Passold <jacson@ethymos.com.br>
2016-07-05 01:36:14 -0300

    fix indent

--- themes/generic/page-author-pautas.php
+++ themes/generic/page-author-pautas.php
@@ -26,64 +26,64 @@ global $user_display;
 						</p>
 					</form>
 				</div>
-<form method="get">
-	<label for="per-page"><?php echo __('Pautas por Página:' , 'delibera'); ?></label>
-	<select id="per-page" name="per-page"	onchange='if(this.value != 0) { this.form.submit(); }' >
-		<option value="5" <?php echo $per_page=='5' ? 'selected' : '' ;?> >5</option>
-		<option value="10" <?php echo $per_page=='10' ? 'selected' : '' ;?> >10</option>
-		<option value="20" <?php echo $per_page=='20' ? 'selected' : '' ;?> >20</option>
-	</select>
-</form>
-<a href="<?php echo get_site_url(); ?>/delibera/membros" ><?php _e('Ver todos os Membros' , 'delibera' ); ?></a>
-<a href="<?php echo get_site_url(); ?>/delibera/<?php echo deliberaEncryptor('encrypt', $user->ID); ?>/comentarios" ><? _e('Ver todas os Comentários de' , 'delibera' ); ?> <?php echo $user->display_name; ?></a>
-<p>
-	<div>
-		<?php echo get_avatar( $user->ID ); ?>
-		<h1><?php echo $user->first_name ?></h1>
-	</div>
-	<div>
-		<h2><?php echo 'Pautas:'; ?></h2>
-	</div>
-</p>
+				<form method="get">
+					<label for="per-page"><?php echo __('Pautas por Página:' , 'delibera'); ?></label>
+					<select id="per-page" name="per-page"	onchange='if(this.value != 0) { this.form.submit(); }' >
+						<option value="5" <?php echo $per_page=='5' ? 'selected' : '' ;?> >5</option>
+						<option value="10" <?php echo $per_page=='10' ? 'selected' : '' ;?> >10</option>
+						<option value="20" <?php echo $per_page=='20' ? 'selected' : '' ;?> >20</option>
+					</select>
+				</form>
+				<a href="<?php echo get_site_url(); ?>/delibera/membros" ><?php _e('Ver todos os Membros' , 'delibera' ); ?></a>
+				<a href="<?php echo get_site_url(); ?>/delibera/<?php echo deliberaEncryptor('encrypt', $user->ID); ?>/comentarios" ><? _e('Ver todas os Comentários de' , 'delibera' ); echo ' '.$user->display_name; ?></a>
+				<p>
+					<div>
+						<?php echo get_avatar( $user->ID ); ?>
+						<h1><?php echo $user->first_name ?></h1>
+					</div>
+					<div>
+						<h2><?php echo 'Pautas:'; ?></h2>
+					</div>
+				</p>
 
-	<?php
-	$args = array(
-		'author'					=> $user->ID,
-		'status'					=> 'approve',
-		's'							 => $search,
-		'posts_per_page'	=> $per_page,
-		'post_type'			 => 'pauta',
-		'paged'					 => get_query_var( 'paged' )
-	);
-
-	$author_posts = new WP_Query( $args );
-	?>
-	<div id="user_pager" class="user_pager">
-		<p>
-			<?php echo \Delibera\Theme\UserDisplay::getPaginator( $author_posts->max_num_pages, $paged ); ?>
-		</p>
-	</div>
-	<?php
-	foreach( $author_posts->posts as $post )
-	{
-		?>
-		<p>
-			<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-				<?php the_title('<h3>', '</h3>'); ?>
-			</a>
-		</p>
-		<?php
-		the_excerpt();
-		echo '<br>';
-		echo delibera_get_situacao($post->ID)->name;
-	}
-
-	?>
-		<div id="user_pager" class="user_pager">
-			<p>
-				<?php echo \Delibera\Theme\UserDisplay::getPaginator( $author_posts->max_num_pages , $per_page ); ?>
-			</p>
-		</div>
+				<?php
+				$args = array(
+					'author'					=> $user->ID,
+					'status'					=> 'approve',
+					's'							 => $search,
+					'posts_per_page'	=> $per_page,
+					'post_type'			 => 'pauta',
+					'paged'					 => get_query_var( 'paged' )
+				);
+			
+				$author_posts = new WP_Query( $args );
+				?>
+				<div id="user_pager" class="user_pager">
+					<p>
+						<?php echo \Delibera\Theme\UserDisplay::getPaginator( $author_posts->max_num_pages, $paged ); ?>
+					</p>
+				</div>
+				<?php
+				foreach( $author_posts->posts as $post )
+				{
+					?>
+					<p>
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+							<?php the_title('<h3>', '</h3>'); ?>
+						</a>
+					</p>
+					<?php
+					the_excerpt();
+					echo '<br>';
+					echo delibera_get_situacao($post->ID)->name;
+				}
+			
+				?>
+				<div id="user_pager" class="user_pager">
+					<p>
+						<?php echo \Delibera\Theme\UserDisplay::getPaginator( $author_posts->max_num_pages , $per_page ); ?>
+					</p>
+				</div>
 			</main><!-- #main -->
 		</div>
 	</div><!-- #content -->
