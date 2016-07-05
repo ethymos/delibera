3b5bda3dc3e598026acb52898e5adec1fd0fa3f6
Jacson Passold <jacson@ethymos.com.br>
2016-07-04 23:42:51 -0300

    report with pauta, and using wp loop

--- delibera_relatorio_xls.php
+++ delibera_relatorio_xls.php
@@ -20,27 +20,51 @@ $pautas_query = new WP_Query(array(
 	'post_status' => 'publish',
 	'posts_per_page' => -1
 ));
-$pautas = $pautas_query->get_posts();
 
 $comments = array();
+/* @var $pauta WP_POST */
 
-foreach ($pautas as $pauta) {
-    $comments = array_merge(
-        $comments,
-        delibera_get_comments($pauta->ID, array('discussao', 'encaminhamento', 'encaminhamento_selecionado', 'resolucao'))
-    );
-}
-
-foreach ($comments as $comment) {
-    $situacao = delibera_get_situacao($comment->comment_post_ID);
-    
-    $comment->pauta_title = get_the_title($comment->comment_post_ID);
-    $comment->pauta_status = $situacao->name;
-    $comment->type = delibera_get_comment_type_label($comment, false, false);
-    $comment->link = get_comment_link($comment);
-    $comment->concordaram = (int) get_comment_meta($comment->comment_ID, 'delibera_numero_curtir', true);
-    $comment->discordaram = (int) get_comment_meta($comment->comment_ID, 'delibera_numero_discordar', true);
-    $comment->votes_count = (int) get_comment_meta($comment->comment_ID, "delibera_comment_numero_votos", true);;
+if($pautas_query->have_posts())
+{
+	global $post;
+	while ($pautas_query->have_posts())
+	{
+		$pautas_query->the_post();
+		$pauta = $post;	
+		
+		$situacao = delibera_get_situacao($pauta->ID);
+		$comment_fake = new stdClass();
+		$comment_fake->pauta_title = get_the_title($pauta->ID);
+		$comment_fake->pauta_status = $situacao->name;
+		$comment_fake->type = 'Pauta';
+		$comment_fake->link = get_permalink($pauta);
+		$comment_fake->concordaram = (int) get_post_meta($pauta->ID, 'delibera_numero_curtir', true);
+		$comment_fake->discordaram = (int) get_post_meta($pauta->ID, 'delibera_numero_discordar', true);
+		$comment_fake->votes_count = (int) get_post_meta($pauta->ID, "delibera_numero_comments_votos", true);
+		$comment_fake->comment_author = get_the_author();
+		$comment_fake->comment_author_email = get_the_author_meta('email', $pauta->post_author);
+		$comment_fake->comment_content = get_the_content();
+		
+		$comment_tmp = delibera_get_comments($pauta->ID, array('discussao', 'encaminhamento', 'encaminhamento_selecionado', 'resolucao'));
+	    $comments = array_merge(
+	        $comments,
+	    	array($comment_fake),
+	        $comment_tmp
+	    );
+	}
+	foreach ($comments as $comment) //TODO with this get bigger, we will have memory problem, better read pauta, comments and write, read next...
+	{
+		if($comment->type == 'Pauta') continue;
+		
+		$situacao = delibera_get_situacao($comment->comment_post_ID);
+	    $comment->pauta_title = get_the_title($comment->comment_post_ID);
+	    $comment->pauta_status = $situacao->name;
+	    $comment->type = delibera_get_comment_type_label($comment, false, false);
+	    $comment->link = get_comment_link($comment);
+	    $comment->concordaram = (int) get_comment_meta($comment->comment_ID, 'delibera_numero_curtir', true);
+	    $comment->discordaram = (int) get_comment_meta($comment->comment_ID, 'delibera_numero_discordar', true);
+	    $comment->votes_count = (int) get_comment_meta($comment->comment_ID, "delibera_comment_numero_votos", true);
+	}
 }
 
 header('Pragma: public');
@@ -55,12 +79,12 @@ header('Content-Disposition: attachment; filename=relatorio.xls');
 echo utf8_decode("
 <table>
     <tr>
-        <td>Pauta</td>
+        <td>Título da Pauta</td>
         <td>Situação</td>
-        <td>Nome</td>
+        <td>Nome do Autor</td>
         <td>E-mail</td>
-        <td>Tipo de comentário</td>
-        <td>Comentário</td>
+        <td>Tipo (Pauta ou tipo de comentário)</td>
+        <td>Conteúdo</td>
         <td>Link</td>
         <td>Concordaram</td>
         <td>Discordaram</td>
