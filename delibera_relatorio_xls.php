f0afa7be68d7a245bf1c1f8ac1b2804c8b90a458
Jacson Passold <jacson@ethymos.com.br>
2016-07-08 15:46:23 -0300

    add created time to report file name

--- delibera_relatorio_xls.php
+++ delibera_relatorio_xls.php
@@ -83,7 +83,7 @@ header("Expires: 0");
 header('Content-Transfer-Encoding: none');
 header('Content-Type: application/vnd.ms-excel; charset=UTF-8'); // This should work for IE & Opera
 header("Content-type: application/x-msexcel; charset=UTF-8"); // This should work for the rest
-header('Content-Disposition: attachment; filename='.date('Ymd').'_'.__('relatorio', 'delibera').'.xls');
+header('Content-Disposition: attachment; filename='.date('Ymd_His').'_'.__('relatorio', 'delibera').'.xls');
 
 
 ob_start(); ?>
